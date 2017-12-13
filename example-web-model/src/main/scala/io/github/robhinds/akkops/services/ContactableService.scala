2e03c51e1f85c35575a9ef9a9c8517e468db3ad4
robhinds <robhinds@gmail.com>
2017-12-13 15:21:20 +0000

    updating to use futures in routes

--- example-web-model/src/main/scala/io/github/robhinds/akkops/services/ContactableService.scala
+++ example-web-model/src/main/scala/io/github/robhinds/akkops/services/ContactableService.scala
@@ -4,11 +4,13 @@ import io.github.robhinds.akkops.model.core.Actions.GetUserRequest
 import io.github.robhinds.akkops.model.core.Response.Response
 import io.github.robhinds.akkops.model.domain.{Channel, Contactable, User}
 
+import scala.concurrent.Future
+
 trait ContactableService {
 
-  def getAllUsers: Response[Seq[User]]
-  def getAllChannels: Response[Seq[Channel]]
-  def getContactable(request: GetUserRequest): Response[Contactable]
+  def getAllUsers: Future[Response[Seq[User]]]
+  def getAllChannels: Future[Response[Seq[Channel]]]
+  def getContactable(request: GetUserRequest): Future[Response[Contactable]]
 
 }
 
--- web-app/src/main/scala/io/github/robhinds/akkops/routing/ResponseHandler.scala
+++ web-app/src/main/scala/io/github/robhinds/akkops/routing/ResponseHandler.scala
@@ -2,27 +2,31 @@ package io.github.robhinds.akkops.routing
 
 import akka.http.scaladsl.model.{StatusCode, StatusCodes}
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.StandardRoute
 import io.circe.{Encoder, Json}
 import io.github.robhinds.akkops.model.core.Errors.AkkOpError
 import io.github.robhinds.akkops.model.core.Response.Response
+
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
 trait ResponseHandler {
   this: ResponseWrapperEncoder =>
   import io.circe.syntax._
   import io.circe.generic.auto._
 
-  def respond[A](response: Response[A], successStatusCode: StatusCode = StatusCodes.OK)
-    (implicit ee: Encoder[AkkOpError], te: Encoder[A]) = {
-    response match {
-      case Left(er) => complete(
-        StatusCode.int2StatusCode(er.statusCode),
-        wrap(StatusCode.int2StatusCode(er.statusCode), er.asJson).toString
-      )
-      case Right(a) => complete(
-        successStatusCode,
-        wrap(successStatusCode, a.asJson).toString
-      )
-    }
+  def respond[A](response: Future[Response[A]], successStatusCode: StatusCode = StatusCodes.OK)
+    (implicit ee: Encoder[AkkOpError], te: Encoder[A]): StandardRoute = {
+    complete(
+      response map {
+        case Left(er) =>
+          (StatusCode.int2StatusCode(er.statusCode),
+          wrap(StatusCode.int2StatusCode(er.statusCode), er.asJson).toString)
+        case Right(a) =>
+          (successStatusCode,
+          wrap(successStatusCode, a.asJson).toString)
+      }
+    )
   }
 }
 
