2cb3d93a1558f47214308f526b0500b7c34146ed
Rob Hinds <robhinds@gmail.com>
2018-05-05 21:16:45 +0100

    response helper methods

--- web-model/src/main/scala/io/github/robhinds/akkops/model/core/Response.scala
+++ web-model/src/main/scala/io/github/robhinds/akkops/model/core/Response.scala
@@ -1,9 +1,16 @@
 package io.github.robhinds.akkops.model.core
 
-import io.github.robhinds.akkops.model.core.Errors.ErrorResponse
+import io.github.robhinds.akkops.model.core.Errors.{ErrorResponse, NotFound}
 
 object Response {
 
   type Response[T] = Either[ErrorResponse, T]
+
+  def success[T](t: T): Response[T] = Right(t)
+  def failure[T](e: ErrorResponse): Response[T] = Left(e)
+  def fromOption[T](o: Option[T], e: ErrorResponse = NotFound("Lookup not found")): Response[T] = o match {
+    case Some(x) => success(x)
+    case _ => failure[T](e)
+  }
 
 }
