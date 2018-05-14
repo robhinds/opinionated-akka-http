73eaef690f5478ab34e366fb9ca47213b186af88
Rob Hinds <robhinds@gmail.com>
2018-05-14 20:49:06 +0100

    adding some auth stuff tests

--- web-app/src/test/scala/io/github/robhinds/akkops/auth/directives/AuthorizationDirectivesSpec.scala
+++ web-app/src/test/scala/io/github/robhinds/akkops/auth/directives/AuthorizationDirectivesSpec.scala
@@ -14,14 +14,19 @@ class AuthorizationDirectivesSpec extends FlatSpec with Matchers with ScalatestR
   val smallRoute = Route.seal(
     get {
       path("test") {
-        roleOf("ADMIN") { user =>
+        roleOf("ADMIN") { _ =>
           complete {
-            "not gonna happens"
+            "logged in admin user"
           }
         }
       } ~
       path("login") {
         login(UserSession(UUID.randomUUID, "user", List("ADMIN"))) {
+          complete("Ok")
+        }
+      } ~
+      path("logout") {
+        logout {
           complete("Ok")
         }
       }
@@ -34,15 +39,48 @@ class AuthorizationDirectivesSpec extends FlatSpec with Matchers with ScalatestR
     }
   }
 
-//  "logging in as admin" should "allow user to access endpoints" in {
-//    import akka.http.scaladsl.model.headers._
-//    Get("/login") ~> smallRoute ~> check {
-//      val cookies = response.headers.collect {
-//        case c: `Set-Cookie` => c.cookie
-//      }
-//      println(cookies)
-//      responseAs[String] shouldEqual "The supplied authentication is not authorized to access this resource"
-//    }
-//  }
+  "logging in as admin" should "allow user to access endpoints" in {
+    import akka.http.scaladsl.model.headers._
+
+    Get("/login") ~> smallRoute ~> check {
+      val c = response.headers.collect {
+        case c: `Set-Cookie` => c.cookie
+      }
+
+      println(c)
+      val sessionData = c.head
+      val refreshToken = c(1)
+
+
+      Get("/test") ~> addHeaders(Cookie(sessionData.name, sessionData.value), Cookie(refreshToken.name, refreshToken.value)) ~> smallRoute ~> check {
+        responseAs[String] shouldEqual "logged in admin user"
+      }
+    }
+  }
+
+  "logging out" should "clear cookies" in {
+    import akka.http.scaladsl.model.headers._
+
+    Get("/login") ~> smallRoute ~> check {
+      val c = response.headers.collect {
+        case c: `Set-Cookie` => c.cookie
+      }
+
+      val sessionData = c.head
+      val refreshToken = c(1)
+
+      Get("/logout") ~> addHeaders(Cookie(sessionData.name, sessionData.value), Cookie(refreshToken.name, refreshToken.value)) ~> smallRoute ~> check {
+        val c = response.headers.collect {
+          case c: `Set-Cookie` => c.cookie
+        }
+
+        val sessionDataLoggedOut = c.head
+        val refreshTokenLoggedOut = c(1)
+
+        sessionDataLoggedOut.value shouldBe "deleted"
+        refreshTokenLoggedOut.value shouldBe "deleted"
+      }
+    }
+  }
 
 }
